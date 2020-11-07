a5a76b13c6921dd7d31723f8b6710517771360a3
stakagi <Shinichi.Takagi@ha.ax>
2020-11-07 16:46:22 +0200

    CopyPeople component added (currently just some hard-coded data)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import CopyPeople from '@/components/CopyPeople.vue' // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    CopyPeople
   }
 })
 export default class Home extends Vue {}
